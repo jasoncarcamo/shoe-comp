d53c6069d031b3176ef6eb426749987996e79e08
Jason Carcamo <jasoncarcamo30@yahoo.com>
2019-09-30 14:36:30 -0400

    Ready for user feedback

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,16 +2,20 @@ import React from "react";
 import "./footer.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faFacebookF} from "@fortawesome/free-brands-svg-icons";
-import {faInstagram} from "@fortawesome/free-brands-svg-icons"
-import {faTwitter} from "@fortawesome/free-brands-svg-icons"
+import {faInstagram} from "@fortawesome/free-brands-svg-icons";
+import {faTwitter} from "@fortawesome/free-brands-svg-icons";
+
 
 export default class Footer extends React.Component{
     render(){
         return (
             <footer>
-                <FontAwesomeIcon id="footer-facebook" icon={faFacebookF}></FontAwesomeIcon>
-                <FontAwesomeIcon id="footer-instagram" icon={faInstagram}></FontAwesomeIcon>
-                <FontAwesomeIcon id="footer-twitter" icon={faTwitter}></FontAwesomeIcon>
+
+                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon id="footer-facebook" icon={faFacebookF}></FontAwesomeIcon></a>
+
+                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon id="footer-instagram" icon={faInstagram}></FontAwesomeIcon></a>
+
+                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon id="footer-twitter" icon={faTwitter}></FontAwesomeIcon></a>
             </footer>
         );
     }
